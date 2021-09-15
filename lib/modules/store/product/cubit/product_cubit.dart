3d7d41690d27de6abf72dabc9de64b1cea6d8def
doitduri <kdle_@naver.com>
2021-09-16 00:51:15 +0900

    📰 product cubit
    - repository
    - cubit, state 추가
    - 상품 상세정보 가져오기

---
+++ lib/modules/store/product/cubit/product_cubit.dart
@@ -0,0 +1,29 @@
+import 'package:aroundus_app/repositories/product_repository/product_repository.dart';
+import 'package:aroundus_app/support/networks/api_result.dart';
+import 'package:aroundus_app/support/networks/network_exceptions.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'product_state.dart';
+
+class ProductCubit extends Cubit<ProductState> {
+  ProductCubit(this._productRepository) : super(const ProductState());
+
+  final ProductRepository _productRepository;
+
+  Future<void> getProductDetail(String productId) async {
+    ApiResult<Product> apiResult =
+        await _productRepository.getProductDetail(productId);
+
+    apiResult.when(success: (Product? productResponse) {
+      print(productResponse!);
+      emit(state.copyWith(
+        products: [productResponse],
+        isLoaded: true,
+        isLoading: false,
+      ));
+    }, failure: (NetworkExceptions? error) {
+      emit(state.copyWith(error: error));
+    });
+  }
+}
