92e59c3a1361e57255bc1b320c5a151be34bc748
doitduri <kdle_@naver.com>
2021-09-23 16:52:26 +0900

    🚧 web deploy

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -2,9 +2,11 @@ import 'package:aroundus_app/modules/authentication/authentication.dart';
 import 'package:aroundus_app/routes.dart';
 import 'package:aroundus_app/support/networks/dio_client.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_web_frame/flutter_web_frame.dart';
 import 'package:sizer/sizer.dart';
 
 import 'modules/authentication/bloc/authentication_bloc.dart';
@@ -25,55 +27,62 @@ class _AppViewState extends State<AppView> {
   @override
   Widget build(BuildContext context) {
     return Sizer(builder: (context, orientation, deviceType) {
-      return MaterialApp(
-        theme: theme,
-        navigatorKey: _navigatorKey,
-        builder: (context, child) {
-          DioClient.authenticationBloc =
-              BlocProvider.of<AuthenticationBloc>(context);
+      return  FlutterWebFrame(
+        builder: (context) {
+          return MaterialApp(
+            theme: theme,
+            navigatorKey: _navigatorKey,
+            builder: (context, child) {
+              DioClient.authenticationBloc =
+                  BlocProvider.of<AuthenticationBloc>(context);
 
-          return MediaQuery(
-            data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
-            //set desired text scale factor here
-            child: buildMultiBlocListener(child!),
+              return MediaQuery(
+                data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+                //set desired text scale factor here
+                child: buildMultiBlocListener(child!),
+              );
+            },
+            localizationsDelegates: [
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+            ],
+            supportedLocales: [
+              const Locale('ko', 'KR'),
+              const Locale('en', 'US'),
+            ],
+            initialRoute: SplashPage.routeName,
+            routes: routes,
           );
         },
-        localizationsDelegates: [
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-        ],
-        supportedLocales: [
-          const Locale('ko', 'KR'),
-          const Locale('en', 'US'),
-        ],
-        initialRoute: SplashPage.routeName,
-        routes: routes,
+        maximumSize: Size(475.0, 812.0),
+        enabled: kIsWeb,
+        backgroundColor: Colors.grey,
       );
-    });
-  }
-
-  MultiBlocListener buildMultiBlocListener(Widget child) {
-    return MultiBlocListener(listeners: [
-      BlocListener<AuthenticationBloc, AuthenticationState>(
-        listener: (context, state) {
-          switch (state.status) {
-            case AuthenticationStatus.profile:
-              _navigator!.pushNamedAndRemoveUntil(
-                  'signup_nickname_page', (route) => false);
-              break;
-            case AuthenticationStatus.authenticated:
-              _navigator!
-                  .pushNamedAndRemoveUntil('home_screen', (route) => false);
-              break;
-            case AuthenticationStatus.unauthenticated:
-              _navigator!.pushNamedAndRemoveUntil(
-                  'login_home_screen', (route) => false);
-              break;
-            default:
-              break;
-          }
-        },
-      ),
-    ], child: child);
-  }
+    },
+  );
 }
+
+MultiBlocListener buildMultiBlocListener(Widget child) {
+  return MultiBlocListener(listeners: [
+    BlocListener<AuthenticationBloc, AuthenticationState>(
+      listener: (context, state) {
+        switch (state.status) {
+          case AuthenticationStatus.profile:
+            _navigator!.pushNamedAndRemoveUntil(
+                'signup_nickname_page', (route) => false);
+            break;
+          case AuthenticationStatus.authenticated:
+            _navigator!
+                .pushNamedAndRemoveUntil('home_screen', (route) => false);
+            break;
+          case AuthenticationStatus.unauthenticated:
+            _navigator!.pushNamedAndRemoveUntil(
+                'login_home_screen', (route) => false);
+            break;
+          default:
+            break;
+        }
+      },
+    ),
+  ], child: child);
+}}
