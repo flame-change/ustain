04db24ce8d99401bdf7bdac7b7457c126e33222c
doitduri <kdle_@naver.com>
2021-10-07 22:39:46 +0900

    🎨 기본 테마 변경
    - button
    - theme

--- lib/support/base_component/plain_button.dart
+++ lib/support/base_component/plain_button.dart
@@ -1,12 +1,25 @@
+import 'package:aroundus_app/support/style/size_util.dart';
+import 'package:aroundus_app/support/style/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sizer/flutter_sizer.dart';
 
 class PlainButton extends StatefulWidget {
-
-  PlainButton({required this.text, this.size, required this.onPressed});
+  PlainButton({
+    required this.onPressed,
+    required this.text,
+    this.width,
+    this.height,
+    this.color,
+    this.textColor,
+    this.borderColor,
+  });
 
   final String text;
-  final int? size;
+  final int? width;
+  final double? height;
+  final Color? color;
+  final Color? borderColor;
+  final Color? textColor;
   final VoidCallback? onPressed;
 
   @override
@@ -14,18 +27,38 @@ class PlainButton extends StatefulWidget {
 }
 
 class _PlainButtonState extends State<PlainButton> {
-  String get _text => this.widget.text;
-  int? get _size => this.widget.size==null?100:this.widget.size;
+  String get text => this.widget.text;
+
+  int? get width => this.widget.width == null ? 100 : this.widget.width;
+
+  double? get height => this.widget.height == null ? 6 : this.widget.height;
+
+  Color? get color =>
+      this.widget.color == null ? Colors.black : this.widget.color;
+
+  Color? get borderColor =>
+      this.widget.borderColor == null ? null : this.widget.borderColor;
+
+  Color? get textColor =>
+      this.widget.textColor == null ? Colors.white : this.widget.textColor;
+
   VoidCallback? get _onPressed => this.widget.onPressed;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: Adaptive.w(_size!),
+      width: sizeWith(width!),
+      // color: color,
+      decoration: BoxDecoration(
+          color: color,
+          border:
+              Border.all(color: borderColor == null ? color! : borderColor!)),
+      height: Adaptive.h(height!),
       child: MaterialButton(
         onPressed: _onPressed,
-        color: Colors.grey,
-        child: Text(_text),
+        textColor:  borderColor == null ? textColor:borderColor,
+        elevation: 0,
+        child: Text(text),
       ),
     );
   }
