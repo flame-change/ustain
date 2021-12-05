1322071d78f35a4098c7bb6afd7d90365786e15e
no-gari <sofaissofa@icloud.com>
2021-12-05 17:00:45 +0900

    my-page fix

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -1,6 +1,6 @@
 const bool isProduction = bool.fromEnvironment('dart.vm.product');
 
-const devConfig = {'baseUrl': 'http://localhost:8000/'};
-const productionConfig = {'baseUrl': 'http://localhost:8000/'};
+const devConfig = {'baseUrl': 'https://api.ustain.be/'};
+const productionConfig = {'baseUrl': 'https://api.ustain.be/'};
 
 final environment = isProduction ? productionConfig : devConfig;
